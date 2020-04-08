756f76c5b7547d56915891cd1465f82e0f0c2e16
Skideau <BarbarossaSkido@gmail.com>
2020-04-08 14:15:38 -0400

    Adding the first element of "!devoir" function : listing registered homework to comes

--- bot_rewrite.py
+++ bot_rewrite.py
@@ -1,31 +1,20 @@
 import discord
 from discord.ext import commands
 import os
-from pymongo import MongoClient
-import datetime
-import json
+
 
 TOKEN = "Njg5MTg0MTU5MDcxMzM4NTAy.Xm_K3g.-NejIbWzgY9SNV5lUf5LQAMPc4s"
 
 schoolBot = commands.Bot(command_prefix='!')
 
-# Data about the mongo database used
-# Needs to have a predefined mongo database
-with open("source_sloth/config/dbStruct.json", "r") as dbConfig:
-    dbStruct = json.load(dbConfig)
-
-# Connect and access to mongo database "schoolBot"
-mongoCli = MongoClient(dbStruct['db_address'], dbStruct['db_port'])
-db = mongoCli[dbStruct['db_name']]
-
-# Access example to the collection called "devoir" in database "schoolBot"
-devoirDB = db[dbStruct['db_collections']['homework']]
-# Getting back all "devoir" corresponding to the "subject" = "math"
-# Fake data were registered for test purpose
-dateTest = datetime.datetime(2020, 4, 9)
-devoirs = devoirDB.find({dbStruct['db_collections']['homework_fields']['deadline']:{'$gt':dateTest}}).sort([(dbStruct['db_collections']['homework_fields']['target'], 1)])
-for dev in devoirs:
-    print(dev)
+# # Access example to the collection called "devoir" in database "schoolBot"
+# devoirDB = db[dbStruct['db_collections']['homework']]
+# # Getting back all "devoir" corresponding to the "subject" = "math"
+# # Fake data were registered for test purpose
+# dateTest = datetime.datetime(2020, 4, 9)
+# devoirs = devoirDB.find({dbStruct['db_collections']['homework_fields']['deadline']:{'$gt':dateTest}}).sort([(dbStruct['db_collections']['homework_fields']['target'], 1)])
+# for dev in devoirs:
+#     print(dev)
 
 # Three functions to manage the COGs 
 @schoolBot.command()
@@ -86,4 +75,4 @@ for filename in os.listdir("source_sloth/cogs"):
 
 # print(", ".join(schoolBot.cogs.keys()))
 
-# schoolBot.run(TOKEN)
+schoolBot.run(TOKEN)
--- cogs/test.py
+++ cogs/test.py
@@ -1,10 +1,30 @@
 import discord
 from discord.ext import commands
+from pymongo import MongoClient
+import datetime
+import json
 
 class Example(commands.Cog):
-    def __init__(self, client):
+    def __init__(self, schoolBot):
         super().__init__()
-        self.client = client
+        self.schoolBot = schoolBot
+
+        # Data about the mongo database used
+        # Needs to have a predefined mongo database
+        with open("source_sloth/config/dbStruct.json", "r") as dbConfig:
+            self.dbStruct = json.load(dbConfig)
+
+        # Connect and access to mongo database "schoolBot"
+        self.mongoCli = MongoClient(self.dbStruct['db_address'], self.dbStruct['db_port'])
+        self.mDB = self.mongoCli[self.dbStruct['db_name']]
+
+        # # Access example to the collection 'homework' in database 'db'
+        # devoirDB = db[dbStruct['db_collections']['homework']]
+        # # Fake data were registered for test purpose
+        # dateTest = datetime.datetime(2020, 4, 9)
+        # devoirs = devoirDB.find({dbStruct['db_collections']['homework_fields']['deadline']:{'$gt':dateTest}}).sort([(dbStruct['db_collections']['homework_fields']['target'], 1)])
+        # for dev in devoirs:
+        #     print(dev)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -14,5 +34,27 @@ class Example(commands.Cog):
     async def ping(self, ctx):
         await ctx.send("Pong")
 
-def setup(client):
-    client.add_cog(Example(client))
+    homeworkDesc = """Fonction pour manipuler les devoirs
+    --> listage, ajout, modification, suppression"""
+    @commands.command(pass_context=True, description=homeworkDesc)
+    async def devoir(self, ctx):
+        dMsg = ctx.message
+
+        # Connection to the homework database
+        hmwDB = self.mDB[self.dbStruct['db_collections']['homework']]
+        # Get all homework with a deadline greater than the current time
+        homeworks = hmwDB.find({self.dbStruct['db_collections']['homework_fields']['deadline']: {'$gte':datetime.datetime.now()}})
+
+        # Formatting the listing homework message before sending it
+        formatedHmwList = "```"
+        for homework in homeworks:
+            formatedHmwList += f"{homework[self.dbStruct['db_collections']['homework_fields']['deadline']]}\n\t- {homework[self.dbStruct['db_collections']['homework_fields']['subject']]} : devoir\n"
+        formatedHmwList += "```"
+
+        # Sending the msg
+        hmwListMsg  = await ctx.send(formatedHmwList)
+
+        # TODO: Add Reactions to the message : add, edit, delete
+
+def setup(schoolBot):
+    schoolBot.add_cog(Example(schoolBot))
