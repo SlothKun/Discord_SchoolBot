53dfc538ad549019cb7a1a9591d1badf5affce0d
Skideau <BarbarossaSkido@gmail.com>
2020-04-08 12:33:25 -0400

    Added pymongo support to connect to a MongoDB database

--- bot_rewrite.py
+++ bot_rewrite.py
@@ -1,15 +1,43 @@
 import discord
 from discord.ext import commands
 import os
+from pymongo import MongoClient
+import datetime
 
 TOKEN = "Njg5MTg0MTU5MDcxMzM4NTAy.Xm_K3g.-NejIbWzgY9SNV5lUf5LQAMPc4s"
 
-client = commands.Bot(command_prefix='!')
+schoolBot = commands.Bot(command_prefix='!')
 
-@client.command()
+# Data about the mongo database used
+# Needs to have a predefined mongo database
+mongoDBAddress = 'localhost'
+mongoDBPort = 27017
+mongoDBName = 'schoolBot'
+mongoDBCollections = {
+    'professeur': 'professeur',
+    'eleve': 'eleve',
+    'devoir': 'devoir',
+    'correction':'correction'
+}
+
+# Connect and access to mongo database "schoolBot"
+mongoCli = MongoClient(mongoDBAddress, mongoDBPort)
+db = mongoCli[mongoDBName]
+
+# # Access example to the collection called "devoir" in database "schoolBot"
+# devoirDB = db[mongoDBCollections['devoir']]
+# # Getting back all "devoir" corresponding to the "subject" = "math"
+# # Fake data were registered for test purpose
+# dateTest = datetime.datetime(2020, 4, 9)
+# devoirs = devoirDB.find({'deadline':{'$gt':dateTest}}).sort([('cible', 1)])
+# for dev in devoirs:
+#     print(dev)
+
+# Three functions to manage the COGs 
+@schoolBot.command()
 async def load(ctx, extension):
     try:
-        client.load_extension(f'cogs.{extension}')
+        schoolBot.load_extension(f'cogs.{extension}')
     except commands.ExtensionNotFound:
         print(f"Extension {extension} is not found")
     except commands.ExtensionFailed:
@@ -19,22 +47,22 @@ async def load(ctx, extension):
     except commands.ExtensionAlreadyLoaded:
         pass
 
-@client.command()
+@schoolBot.command()
 async def unload(ctx, extension):
     try:
-        client.unload_extension(f'cogs.{extension}')
+        schoolBot.unload_extension(f'cogs.{extension}')
     except commands.ExtensionNotLoaded as enl:
         print(f"Extension {extension} is not loaded")
     
 
-@client.command()
+@schoolBot.command()
 async def reload(ctx, extension):
     try:
-        client.reload_extension(f'cogs.{extension}')
+        schoolBot.reload_extension(f'cogs.{extension}')
     except commands.ExtensionNotLoaded as enl:
         print(f"Extension {extension} is not loaded")
         try:
-            client.load_extension(f'cogs.{extension}')
+            schoolBot.load_extension(f'cogs.{extension}')
         except commands.ExtensionNotFound:
             print(f"Extension {extension} is not found")
         except commands.ExtensionFailed:
@@ -50,10 +78,11 @@ async def reload(ctx, extension):
     except commands.NoEntryPointError as enl:
         print(f"Extension {extension} can not be loaded")
 
+# Automatic load of all COGs listed in folder './cogs'
 for filename in os.listdir("./cogs"):
     if filename.endswith('.py'):
         try:
-            client.load_extension(f'cogs.{filename[:-3]}')
+            schoolBot.load_extension(f'cogs.{filename[:-3]}')
         except commands.ExtensionNotFound:
             print(f"Extension {filename[:-3]} is not found")
         except commands.ExtensionFailed:
@@ -61,6 +90,6 @@ for filename in os.listdir("./cogs"):
         except commands.NoEntryPointError:
             print(f"Extension {filename[:-3]} can not be loaded")
 
-# print(", ".join(client.cogs.keys()))
+# print(", ".join(schoolBot.cogs.keys()))
 
-client.run(TOKEN)
+schoolBot.run(TOKEN)
